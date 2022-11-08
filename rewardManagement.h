97c35d185b5a1deb1ca46341b57755b6ad4062b6
Keegan <kgm1328@live.missouristate.edu>
2022-11-07 19:48:13 -0600

    Update rewardManagement.h
    Added shell, can be used as checklist

---
+++ rewardManagement.h
@@ -0,0 +1,7 @@
+/*Reward management class - will connect with mainMenu.cpp
+    this service allows the manager to specify the following
+    - the number of points awarded for a given shopping amount
+    - the gifts that can be redeemed given the total number of reward points
+    - will need function that determine if redeem points are enough
+        - ie. function to check if points cost > customer points
+*/
