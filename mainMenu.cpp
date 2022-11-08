015662a3f8aff5c2d891dd7cfc9b495eefc566d4
Keegan <kgm1328@live.missouristate.edu>
2022-11-07 19:53:37 -0600

    Update mainMenu.cpp
    Added shell, can be used as a checklist

---
+++ mainMenu.cpp
@@ -0,0 +1,12 @@
+/* Main menu class - the driver of the program
+    create a menu system that provides the following options:
+    - customer registration (customerRegistration.h)
+    - customer removal (customerRegistration.h)
+    - product addition (productManagement.h)
+    - product removal (productManagement.h)
+    - shopping (here or make transaction.h?)
+        - if here: all transactions must be saved in a file named "transactions.txt"
+            - check assignment.pdf for format
+    - view customer using customer ID (customerRegistration.h)
+    - redeem rewards (rewardManagement.h)
+*/
