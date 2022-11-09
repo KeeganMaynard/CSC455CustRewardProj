0760043bcaa616c23a737aba43bbfe1a1c4971e5
mgegg016 <72768427+mgegg016@users.noreply.github.com>
2022-11-09 13:22:53 -0600

    changed file structure, moved methods outside of class according to project convention

--- transaction.h
+++ transaction.h
@@ -47,62 +47,62 @@ class transaction {
   void setTotalAmount(float amount) { totalAmount = amount; }
   void setProductIDs(vector<string> ids) { productIDs = ids; }
   void setRewardPoints(int rPoints) { rewardPoints = rPoints; }
+};
 
-  /*
+/*
   This function handles reading from and writing to files for the transaction
   system Parameters: boolean option: option to read or write files, 1 being read
   0 being write string file: the file to be read or wrote too vecto
 
   */
-  vector<transaction> readWriteFiles(bool option, string file,
-                                     vector<transaction> transactions) {
-    fstream myFile;
+vector<transaction> readWriteFiles(bool option, string file,
+                                   vector<transaction> transactions) {
+  fstream myFile;
 
-    // reading
-    if (option == 1) {
-      vector<transaction> transactionList;
-      myFile.open(file);
-    }
-    // writing
-    else if (option == 0) {
-    }
-  }
-
-  vector<string> readFile(string file) {
-    fstream myFile;
-    string line;
+  // reading
+  if (option == 1) {
+    vector<transaction> transactionList;
     myFile.open(file);
-    vector<string> lines;
-    myFile.open(file);
-    while (getline(myFile, line)) {
-      lines.push_back(line);
-    }
-    return lines;
   }
+  // writing
+  else if (option == 0) {
+  }
+}
 
-  vector<string> processProductIDs(string line) {
-    string s;
-    string tmp;
-    stringstream ss(s);
-    vector<string> pIDs;
-    while (getline(ss, tmp, ',')) {
-      pIDs.push_back(tmp);
-    }
-    return pIDs;
+vector<string> readFile(string file) {
+  fstream myFile;
+  string line;
+  myFile.open(file);
+  vector<string> lines;
+  myFile.open(file);
+  while (getline(myFile, line)) {
+    lines.push_back(line);
   }
+  return lines;
+}
 
-  vector<transaction> createTransactions(vector<string> lines) {
-    transaction t;
-    vector<transaction> transactions;
-    int transactionCount = lines.size() % 6;
-    for (int j = 0; j <= lines.size(); j += 6) {
-      t.transactionID = lines[j];
-      t.userID = lines[j + 1];
-      t.productIDs = processProductIDs(lines[j + 2]);
-      t.totalAmount = stof(lines[j + 3]);
-      t.rewardPoints = stoi(lines[j + 4]);
-      transactions.push_back(t);
-    }
-    return transactions;
+vector<string> processProductIDs(string line) {
+  string s;
+  string tmp;
+  stringstream ss(s);
+  vector<string> pIDs;
+  while (getline(ss, tmp, ',')) {
+    pIDs.push_back(tmp);
   }
-};
+  return pIDs;
+}
+
+vector<transaction> createTransactions(vector<string> lines) {
+  transaction t;
+  vector<transaction> transactions;
+  int transactionCount = lines.size() % 6;
+  for (int j = 0; j <= lines.size(); j += 6) {
+    t.transactionID = lines[j];
+    t.userID = lines[j + 1];
+    t.productIDs = processProductIDs(lines[j + 2]);
+    t.totalAmount = stof(lines[j + 3]);
+    t.rewardPoints = stoi(lines[j + 4]);
+    transactions.push_back(t);
+  }
+  return transactions;
+}
