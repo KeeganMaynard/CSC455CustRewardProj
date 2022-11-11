ba729ac0247284b8b9307fde037fef6aeefb0366
Keegan <kgm1328@live.missouristate.edu>
2022-11-11 08:46:53 -0600

    Update mainMenu.cpp
    Resolved some issues created from the last commit to mainMenu

--- mainMenu.cpp
+++ mainMenu.cpp
@@ -18,34 +18,41 @@
 #include <string>
 using namespace std;
 
+void displayOpts();
+customer cust;
+product prod;
+
 int main()
 {
+    cust.initilize();
     bool run = true;
     while (run)
     {
         string option = "";
         displayOpts();
-        cin >> option;
+        getline(cin, option);
 
         switch (stoi(option))
         {
         case 1:
-            registerUser();
+            cust.registerUser();
             break;
         case 2:
-            removeCustomer();
+            cust.removeCustomer();
             break;
         case 3:
+            prod.addProduct();
             break;
         case 4:
+            prod.removeProduct();
             break;
         case 5:
             break;
         case 6:
-            searchCustomer();
+            cust.searchCustomer();
             break;
         case 7:
-            redeemable();
+
             break;
         case 8:
             run = false;
