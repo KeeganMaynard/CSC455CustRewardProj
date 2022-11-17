b7ec859bed6ecc8f7489e5036510e040b5a67d64
Keegan <kgm1328@live.missouristate.edu>
2022-11-17 13:07:45 -0600

    Update customerRegistration.h
    documentation for this class

--- customerRegistration.h
+++ customerRegistration.h
@@ -1,6 +1,6 @@
-/*Customer registration class
+/*Customer registration class - contains the implementation for all customer functions
     author - Keegan Maynard
- */
+*/
 #ifndef CUSTOMERREGISTRATION_H
 #define CUSTOMERREGISTRATION_H
 #include <iostream>
@@ -10,9 +10,11 @@
 #include <regex>
 using namespace std;
 
+// customer class - utilized to store and represent customer information
 class customer
 {
 private:
+  // customer attributes
   string custID;
   string username;
   string fname;
@@ -22,6 +24,7 @@ private:
   int custPoints;
 
 public:
+  // default constructor
   customer(){};
 
   // vector and associated functions to store previously entered and new customers
@@ -72,11 +75,18 @@ public:
   int getPoints() { return custPoints; }
 };
 
+/* void registerUser() - a method of the customer class, this function is utilized to create a new instance
+of the customer class, gather attribute values for this instance, append the instance to the vector of
+customers, and, finally, log the new user to the text file.
+Parameters - none
+Return value - none
+*/
 void customer::registerUser()
 {
   // create a new instance of the customer class
   customer newCustomer;
 
+  // gather newCustomer attributes
   newCustomer.setCustID(generateID());
   newCustomer.setUserName(inputUsername());
   newCustomer.setFName(inputFName());
@@ -94,12 +104,18 @@ void customer::registerUser()
   cout << "\nCustomer successfully entered\n\n";
 }
 
+/* void searchCustomer() - a method of the customer class, this function is utilized to find and display
+customer data based on a user-input username.
+Parameters - none
+Return value - none
+*/
 void customer::searchCustomer()
 {
   string lookUp;
   cout << "Enter the username for the customer you wish to search: ";
   getline(cin, lookUp);
 
+  // test if entered username actually exists
   if (custUNPresent(lookUp))
   {
     // display customer data
@@ -121,12 +137,18 @@ void customer::searchCustomer()
   }
 }
 
+/* void removeCustomer() - a method of the customer class, this function is utilized to find and remove a
+customer based on a user-input username.
+Parameters - none
+Return value - none
+*/
 void customer::removeCustomer()
 {
   string findCust;
   cout << "Enter the username for the customer you wish to remove: ";
   getline(cin, findCust);
 
+  // test if entered username actually exists
   if (custUNPresent(findCust))
   {
     // remove the customer from the vector
@@ -150,12 +172,18 @@ void customer::removeCustomer()
   }
 }
 
-// Function to read the file and fill up the vector
+/* void initilize() - a method of the customer class, this function is utilized to read the customer log
+file, customers.txt, parse the lines, and send the data to parseData().
+Parameters - none
+Return value - none
+*/
 void customer::initilize()
 {
+  // open the log file - customers.txt
   ifstream customersLog;
   customersLog.open("customers.txt");
 
+  // test file access
   if (customersLog.is_open())
   {
     string line;
@@ -166,16 +194,19 @@ void customer::initilize()
       // parse data gathered from line
       if (!line.empty()) // Break on empty line
       {
+        // find the last space in line, store the last word in buffer
         temp = line.substr(line.find_last_of(' ') + 1, line.length());
         buffer = buffer + temp + " ";
       }
       else
       {
+        // send the buffer to parseData() and clear temp and buffer
         parseData(buffer);
         temp = "";
         buffer = "";
       }
     }
+    // close the log file
     customersLog.close();
   }
   else
@@ -184,6 +215,12 @@ void customer::initilize()
   }
 }
 
+/* void parseData(string) - a method of the customer class, this function is utilized to parse a string of
+customer attributes, assign them to an instance of the customer class, and append the instance to the vector
+of customers.
+Parameters - string line, a string representation of customer attributes gathered from the log file
+Return values - none
+*/
 void customer::parseData(string line)
 {
   string loadID, loadUN, loadFirst, loadLast, loadDOB, loadCC, loadPoints;
@@ -215,9 +252,16 @@ void customer::parseData(string line)
   loadPoints = line.substr(0, line.find_first_of(' ') - 1);
   loadCust.setPoints(stoi(loadPoints));
 
+  // append the customer to the vector of customers
   customers.push_back(loadCust);
 }
 
+/* void shutdown() - a method of the customer class, this function is utilized at program termination, it
+clears the log file of all customer data, then writes all customers in the current vector. This function
+ensures removed customers do not remain in the log
+Parameters - none
+Return value - none
+*/
 void customer::shutdown()
 {
   // clear the text file to avoid duplicates
@@ -231,6 +275,11 @@ void customer::shutdown()
   }
 }
 
+/* string generateID() - a recursive method of the customer class, this function is utilized to create the
+unique customer ID for all customers.
+Parameters - none
+Return value - string, the customer ID that will be associated with an account
+*/
 string customer::generateID()
 {
   string newID = "CID";
@@ -238,6 +287,7 @@ string customer::generateID()
   // attach 10 digits at the end
   newID.append(attachNums(currCount));
 
+  // test newID is unique
   if (!custIdPresent(newID))
   {
     return newID;
@@ -248,11 +298,18 @@ string customer::generateID()
   }
 }
 
+/* string inputUsername() - a recursive method of the customer class, this function is utilized to gather
+user input for the username, ensure it is unique, and validate it according to system requirements.
+Parameters - none
+Return value - string, the customer username that will be associated with an account
+*/
 string customer::inputUsername()
 {
   string newUserName = "";
   cout << "Enter your username: ";
   getline(cin, newUserName);
+
+  // test newUserName is unique
   if (!custUNPresent(newUserName))
   {
     // check regex
@@ -263,21 +320,29 @@ string customer::inputUsername()
     else
     {
       cout << "The username must be at least 8 characters followed by at most 3 digits" << endl;
+      // recursive function call if validation failed
       return inputUsername();
     }
   }
   else
   {
     cout << "This username is already associated with an account" << endl;
+    // recursive function call if validation failed
     return inputUsername();
   }
 }
 
+/* string inputFName() - a recursive method of the customer class, this function is utilized to gather
+user input for the first name and validate it according to system requirements.
+Parameters - none
+Return value - string, the customer first name that will be associated with an account
+*/
 string customer::inputFName()
 {
   string newFName = "";
   cout << "Enter your first name: ";
   getline(cin, newFName);
+  // validate the first name input
   if (newFName.length() <= 15 && regex_match(newFName, regex("^[A-Za-z]+$")))
   {
     return newFName;
@@ -285,15 +350,22 @@ string customer::inputFName()
   else
   {
     cout << "The name must be less than 15 characters and cannot contain numbers or special characters" << endl;
+    // recursive function call if validation failed
     return inputFName();
   }
 }
 
+/* string inputLName() - a recursive method of the customer class, this function is utilized to gather
+user input for the last name and validate it according to system requirements.
+Parameters - none
+Return value - string, the customer last name that will be associated with an account
+*/
 string customer::inputLName()
 {
   string newLName = "";
   cout << "Enter your last name: ";
   getline(cin, newLName);
+  // validate the last name input
   if (newLName.length() <= 15 && regex_match(newLName, regex("^[A-Za-z]+$")))
   {
     return newLName;
@@ -301,15 +373,22 @@ string customer::inputLName()
   else
   {
     cout << "The name must be less than 15 characters and cannot contain numbers or special characters" << endl;
+    // recursive function call if validation failed
     return inputLName();
   }
 }
 
+/* string inputDOB() - a recursive method of the customer class, this function is utilized to gather
+user input for the date of birth and validate it according to system requirements.
+Parameters - none
+Return value - string, the customer date of birth that will be associated with an account
+*/
 string customer::inputDOB()
 {
   string newDOB = "";
   cout << "Enter your date of birth: ";
   getline(cin, newDOB);
+  // validate the DOB input
   if (regex_match(newDOB, regex("^(1[0-2]|0[1-9])-(3[01]|[12][0-9]|0[1-9])-[0-9]{4}$")))
   {
     return newDOB;
@@ -317,17 +396,25 @@ string customer::inputDOB()
   else
   {
     cout << "The date of birth must be entered in the MM-DD-YYYY format" << endl;
+    // recursive function call if vaidation failed
     return inputDOB();
   }
 }
 
+/* string inputCCN() - a recursive method of the customer class, this function is utilized to gather user
+input for the credit card number, ensure it is unique, and validate it according to system requirements.
+Parameters - none
+Return value - string, the customer credit card number that will be associated with an account
+*/
 string customer::inputCCN()
 {
   string newCCN = "";
   cout << "Enter your credit card number: ";
   getline(cin, newCCN);
+  // validate the CCN input
   if (matchCCRegex(newCCN))
   {
+    // test if CCN is unique
     if (!custCCNPresent(newCCN))
     {
       return newCCN;
@@ -335,22 +422,30 @@ string customer::inputCCN()
     else
     {
       cout << "This credit card number is already associated with an account" << endl;
+      // recursive function call if CCN already present
       return inputCCN();
     }
   }
   else
   {
     cout << "The credit card number must follow the xxxx-xxxx-xxxx format" << endl;
+    // recursive function call if validation failed
     return inputCCN();
   }
 }
 
+/* int inputPoints() - a recursive method of the customer class, this function is utilized to gather user
+input for the customer's reward points and validate it according to system requirements.
+Parameters - none
+Return value - int, the customer reward points that will be associated with an account
+*/
 int customer::inputPoints()
 {
   string newPoints = "";
   cout << "Enter your current rewards points: ";
   getline(cin, newPoints);
   int convert = stoi(newPoints);
+  // validate the points input
   if (convert > 0)
   {
     return convert;
@@ -358,11 +453,16 @@ int customer::inputPoints()
   else
   {
     cout << "The current rewards points must be greater than 0" << endl;
+    // recursive function call if validation failed
     return inputPoints();
   }
 }
 
-// append the last customer in the vector to the txt file
+/* void logNewUser(int) - a method of the customer class, this function is utilized to append a new customer
+to the log file.
+Parameters - int custNum, the value of the index in the customers vector to retrieve data from
+Return value - none
+*/
 void customer::logNewUser(int custNum)
 {
   string &custID = customers[custNum].custID;
@@ -390,11 +490,21 @@ void customer::logNewUser(int custNum)
   customersLog.close();
 }
 
+/* bool custIdPresent(string) - a method of the customer class, this function is utilized to test if the
+value passed to the function is already present in the vector of customers. This function ensures the
+customer ID is unique.
+Parameters - string value, the customer Id we want to determine if it is already associated with an account
+Return values - true or false
+  returns true if the passed customer Id is found in the vector
+  returns false if the passed customer Id is not found in the vector
+*/
 bool customer::custIdPresent(string value)
 {
+  // for each index in the vector
   for (int i = 0; i < customers.size(); i++)
   {
     string &custIDs = customers[i].custID;
+    // test if custID equal to value
     if (custIDs.compare(value) == 0)
     {
       return true;
@@ -408,6 +518,15 @@ bool customer::custIdPresent(string value)
   return false;
 }
 
+/* bool custCCNPresent(string) - a method of the customer class, this function is utilized to test if the
+value passed to the function is already present in the vector of customers. This function ensures the
+customer credit card number is unique.
+Parameters - string value, the customer credit card number we want to determine if it is already
+associated with an account
+Return values - true or false
+  returns true if the passed customer CCN is found in the vector
+  returns false if the passed customer CCN is not found in the vector
+*/
 bool customer::custCCNPresent(string value)
 {
   for (int i = 0; i < customers.size(); i++)
