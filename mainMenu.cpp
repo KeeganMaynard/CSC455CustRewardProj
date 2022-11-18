5f144764f260653ac7b5a35dd6512e704e2d07eb
Keegan <kgm1328@live.missouristate.edu>
2022-11-18 13:21:19 -0600

    Added new option to menu
    Also changed some wording

--- mainMenu.cpp
+++ mainMenu.cpp
@@ -23,46 +23,52 @@ option based on input.
 Parameters - none
 Return value - 0, signifies successful run of the program.
 */
-int main() {
+int main()
+{
   srand(time(NULL));
   cust.initilize();
   prod.initilize();
   bool run = true;
-  while (run) {
+  while (run)
+  {
     string option = "";
     displayOpts();
     getline(cin, option);
 
     // switch-case based on user input
-    switch (stoi(option)) {
-      case 1:
-        cust.registerUser();
-        break;
-      case 2:
-        cust.removeCustomer();
-        break;
-      case 3:
-        prod.addProduct();
-        break;
-      case 4:
-        prod.removeProduct();
-        break;
-      case 5:
-        trans.shopping(cust, prod);
-        break;
-      case 6:
-        cust.searchCustomer();
-        break;
-      case 7:
-        rew.redeemRewards();
-        break;
-      case 8:
-        cust.shutdown();
-        run = false;
-        break;
-      default:
-        cout << "Error. Invalid option entered. Please try again" << endl;
-        break;
+    switch (stoi(option))
+    {
+    case 1:
+      cust.registerUser();
+      break;
+    case 2:
+      cust.removeCustomer();
+      break;
+    case 3:
+      prod.addProduct();
+      break;
+    case 4:
+      prod.removeProduct();
+      break;
+    case 5:
+      // enter redeem rate
+      break;
+    case 6:
+      trans.shopping(cust, prod);
+      break;
+    case 7:
+      cust.searchCustomer();
+      break;
+    case 8:
+      rew.redeemRewards();
+      break;
+    case 9:
+      cust.shutdown();
+      run = false;
+      break;
+    default:
+      cout << "Error. Invalid option entered. Please try again" << endl;
+      break;
     }
   }
 
@@ -72,15 +78,16 @@ int main() {
 /* void displayOpts() - the purpose of this function is to display the program
 options to the user. Parameters - none Return value - none
 */
-void displayOpts() {
-  cout << "Select the number associated with the opperation you wish to perform"
-       << endl;
-  cout << "[1] Customer registration" << endl;
-  cout << "[2] Customer removal" << endl;
+void displayOpts()
+{
+  cout << "\nSelect the number associated with the opperation you wish to perform" << endl;
+  cout << "[1] Register customer" << endl;
+  cout << "[2] Remove customer" << endl;
   cout << "[3] Product addition" << endl;
   cout << "[4] Product removal" << endl;
-  cout << "[5] Shopping" << endl;
-  cout << "[6] View customer using customer username" << endl;
-  cout << "[7] Redeem rewards" << endl;
-  cout << "[8] End session" << endl;
+  cout << "[5] Set redeem rate" << endl;
+  cout << "[6] Track customer shopping" << endl;
+  cout << "[7] View customer using customer username" << endl;
+  cout << "[8] Redeem customer rewards" << endl;
+  cout << "[9] End session" << endl;
 }
