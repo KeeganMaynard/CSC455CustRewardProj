bc42775ce3564b773b6648da85d6122c15bb455b
Keegan <kgm1328@live.missouristate.edu>
2022-11-18 15:13:30 -0600

    Update rewardManagement.h

--- rewardManagement.h
+++ rewardManagement.h
@@ -15,16 +15,15 @@
 #include <vector>
 
 // rewards class
-class rewards {
- private:
+class rewards
+{
+private:
   // declaration of the 4 main variables of the class
   string gift;
   int giftValue;
   float redeemRate;
-  int moneySpent;
-  int pointsEarned;
 
- public:
+public:
   // vector initialization
   vector<rewards> rews;
 
@@ -36,37 +35,28 @@ class rewards {
   void rewardValues();
   void initilize();
   void parseData(string);
-  int inputMoneySpent();
-  int inputPointsEarned();
   void shutdown();
   void logRewards(int);
   bool validateGift(string);
   bool validateGiftValue(int);
-  bool validateMoneySpent(int);
-  bool validatePointsEarned(int);
   void redeemRewards();
 
   // setters
   void setGift(string newGift) { gift = newGift; }
   void setGiftValue(int newGiftValue) { giftValue = newGiftValue; }
-  void setMoneySpent(int newMoneySpent) { moneySpent = newMoneySpent; }
-  void setPointsEarned(int newPointsEarned) { pointsEarned = newPointsEarned; }
 
   // getters
   string getGift() { return gift; }
   int getGiftValue() { return giftValue; }
-  int getMoneySpent() { return moneySpent; }
-  int getPointsEarned() { return pointsEarned; }
 };
 
 // Declaration and Assignment of the Variables
-void rewards::rewardValues() {
+void rewards::rewardValues()
+{
   rewards newRewards;
 
   newRewards.setGift(inputGift());
   newRewards.setGiftValue(inputGiftValue());
-  newRewards.setMoneySpent(inputMoneySpent());
-  newRewards.setPointsEarned(inputPointsEarned());
 
   rews.push_back(newRewards);
 
@@ -74,179 +64,128 @@ void rewards::rewardValues() {
 }
 
 // filling of the rewards.txt file
-void rewards::initilize() {
+void rewards::initilize()
+{
   ifstream rewardLog;
   rewardLog.open("rewards.txt");
 
-  if (rewardLog.is_open()) {
+  if (rewardLog.is_open())
+  {
     string line;
     string temp;
     string buffer;
-    while (getline(rewardLog, line)) {
-      if (!line.empty()) {
+    while (getline(rewardLog, line))
+    {
+      if (!line.empty())
+      {
         temp = line.substr(line.find_last_of(' ') + 1, line.length());
         buffer = buffer + temp + " ";
-      } else {
+      }
+      else
+      {
         parseData(buffer);
         temp = "";
         buffer = "";
       }
     }
     rewardLog.close();
-  } else {
+  }
+  else
+  {
     cout << "Error reading the rewards file." << endl;
   }
 }
 
 // reading through the rewards.txt file
-void rewards::parseData(string line) {
-  string loadG, loadGV, loadMS, loadPE;
+void rewards::parseData(string line)
+{
+  string loadG, loadGV;
   rewards loadRew;
   loadG = line.substr(0, line.find_first_of(' '));
   loadRew.setGift(loadG);
 
   line = line.substr(line.find_first_of(' ') + 1, line.length());
-  loadGV = line.substr(0, line.find_first_of(' '));
+  loadGV = line.substr(0, line.length());
   loadRew.setGiftValue(stoi(loadGV));
-
-  line = line.substr(line.find_first_of(' ') + 1, line.length());
-  loadMS = line.substr(0, line.find_first_of(' '));
-  loadRew.setMoneySpent(stoi(loadMS));
-
-  line = line.substr(line.find_first_of(' ') + 1, line.length());
-  loadPE = line.substr(0, line.find_first_of(' '));
-  loadRew.setPointsEarned(stoi(loadPE));
 
   rews.push_back(loadRew);
 }
 
 // closing the rewards.txt file
-void rewards::shutdown() {
+void rewards::shutdown()
+{
   ofstream logFile("rewards.txt");
   logFile.close();
 
-  for (int i = 0; i < rews.size(); i++) {
+  for (int i = 0; i < rews.size(); i++)
+  {
     logRewards(i);
   }
 }
 
 // assigning the rewards.txt file to its vector
-void rewards::logRewards(int rewNum) {
+void rewards::logRewards(int rewNum)
+{
   string &gift = rews[rewNum].gift;
   int &giftValue = rews[rewNum].giftValue;
-  int &moneySpent = rews[rewNum].moneySpent;
-  int &pointsEarned = rews[rewNum].pointsEarned;
 
   ofstream rewardsLog;
   rewardsLog.open("rewards.txt", ios::app);
 
   rewardsLog << "Gift: " << gift << endl;
   rewardsLog << "Gift Cost: " << giftValue << endl;
-  rewardsLog << "Money Spent: " << moneySpent << endl;
-  rewardsLog << "Points Earned: " << pointsEarned << endl;
 
   rewardsLog.close();
 }
 
 // inputing the name of the gift, returns the gift name
-string rewards::inputGift() {
+string rewards::inputGift()
+{
   string gift = "";
   cout << "Enter the available reward gift: ";
   getline(cin, gift);
-  if (validateGift(gift)) {
+  if (validateGift(gift))
+  {
     return gift;
-  } else {
-    cout << "Gift must be a valid String";
-    inputGift();
   }
-
-  // Default return statement - will never be hit
-  return "Unable to enter a valid gift name";
+  else
+  {
+    cout << "Gift must be a valid String";
+    return inputGift();
+  }
 }
 
 // inputing the cost of the gift, returns the gift's cost
-int rewards::inputGiftValue() {
+int rewards::inputGiftValue()
+{
   int giftValue = 0;
   cout << "Enter the cost of the rewards gift: ";
   cin >> giftValue;
-  if (validateGiftValue(giftValue)) {
+  if (validateGiftValue(giftValue))
+  {
     return giftValue;
-  } else {
+  }
+  else
+  {
     cout << "Please enter a positive numerical value.";
-    inputGiftValue();
+    return inputGiftValue();
   }
-
-  // Default return statement - will never be hit
-  return 100;
-}
-
-// inputing the customers money spent, returns the value that they spent
-int rewards::inputMoneySpent() {
-  int moneySpent = 0;
-  cout << "Enter the total shopping amount: ";
-  cin >> moneySpent;
-  if (validateMoneySpent(moneySpent)) {
-    return moneySpent;
-  } else {
-    cout << "Please enter a positive numerical value.";
-    inputMoneySpent();
-  }
-
-  // Default return statement - will never be hit
-  return 100;
-}
-
-// inputing the points received for the price earned, returns the amount of
-// points earned
-int rewards::inputPointsEarned() {
-  int pointsEarned = 0;
-  cout << "Enter points earned from shopping: ";
-  cin >> pointsEarned;
-  if (validatePointsEarned(pointsEarned)) {
-    return pointsEarned;
-  } else {
-    cout << "Please eter a positive numerical value. ";
-  }
-
-  // Default return statement - will never be hit
-  return 1;
 }
 
 // Validation of the values
 bool rewards::validateGift(string newGift) { return true; }
 
-bool rewards::validateGiftValue(int newGiftValue) {
-  if (isdigit(newGiftValue)) {
-    if (newGiftValue > 0) {
+bool rewards::validateGiftValue(int newGiftValue)
+{
+  if (isdigit(newGiftValue))
+  {
+    if (newGiftValue > 0)
+    {
       return true;
     }
-  } else {
-    return false;
   }
-
-  // Default return statement - will never be hit
-  return true;
-}
-
-bool rewards::validateMoneySpent(int newMoneySpent) {
-  if (isdigit(newMoneySpent)) {
-    if (newMoneySpent > 0) {
-      return true;
-    }
-  } else {
-    return false;
-  }
-
-  // Default return statement - will never be hit
-  return true;
-}
-
-bool rewards::validatePointsEarned(int newPointsEarned) {
-  if (isdigit(newPointsEarned)) {
-    if (newPointsEarned > 0) {
-      return true;
-    }
-  } else {
+  else
+  {
     return false;
   }
 
@@ -256,7 +195,8 @@ bool rewards::validatePointsEarned(int newPointsEarned) {
 
 // Able to check the customer ID and then redeem their points for whatever
 // specified reward
-void rewards::redeemRewards() {
+void rewards::redeemRewards()
+{
   customer cust;
   string custUN;
   string reward;
@@ -265,20 +205,30 @@ void rewards::redeemRewards() {
   getline(cin, custUN);
   cout << "What gift would you like to reedeem?: " << endl;
   getline(cin, reward);
-  for (int i = 0; i < rews.size(); i++) {
-    if (reward == rews[i].gift) {
+  for (int i = 0; i < rews.size(); i++)
+  {
+    if (reward == rews[i].gift)
+    {
       rewardTemp = rews[i];
-    } else {
+    }
+    else
+    {
     }
   }
-  if (cust.custUNPresent(custUN)) {
-    if (cust.retrievePoints(custUN) > rewardTemp.giftValue) {
+  if (cust.custUNPresent(custUN))
+  {
+    if (cust.retrievePoints(custUN) > rewardTemp.giftValue)
+    {
       int newValue = cust.retrievePoints(custUN) - rewardTemp.giftValue;
       cout << "You have redeemed a Gift!!" << endl;
-    } else {
+    }
+    else
+    {
       cout << "You do not have enough reward points for this gift" << endl;
     }
-  } else {
+  }
+  else
+  {
     cout << "This username is not associated with an account" << endl;
   }
 }
